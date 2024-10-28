173ce22c8baf7533cd6a6c2b384eee124a03b633
hoshima <hoshima@users.noreply.github.com>
2024-10-28 16:20:02 +0900

    chapter 14 - 2
    fix lint error

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -58,6 +58,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
       INSERT INTO invoices (customer_id, amount, status, date)
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (error) {
     return {
       message: "Database Error: Failed to Create Invoice.",
@@ -95,6 +96,7 @@ export async function updateInvoice(
       SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
       WHERE id = ${id}
     `;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (error) {
     return { message: "Database Error: Failed to Update Invoice." };
   }
@@ -108,6 +110,7 @@ export async function deleteInvoice(id: string) {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath("/dashboard/invoices");
     return { message: "Deleted Invoice." };
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (error) {
     return { message: "Database Error: Failed to Delete Invoice." };
   }
